ee405ba9dd36b3f2f5304f7f07e78653b8d95e1c
bastarder <jie.qian@blockheaders.com>
2018-11-15 15:07:48 +0800

    design

--- Unit.js
+++ Unit.js
@@ -3,10 +3,60 @@ class Unit{
     this.uid = 0;
     this.name = 1;
     this.tags = [...opt.tags];
+    this.buffs = [];
+    this.$attrType    = "str",
+    this.$hp = 0;
+    this.$mp = 0;
+    this.$maxHp       = [0,0,0,0];    // 血量最大值
+    this.$maxMp       = [0,0,0,0];    // 魔法最大值
+    this.$atk         = [0,0,0,0];    // 攻击
+    this.$def         = [0,0,0,0];    // 防御
+
+    this.$str         = [0,0,0,0];    // 力量
+    this.$dex         = [0,0,0,0];    // 敏捷
+    this.$int         = [0,0,0,0];    // 智力
+
+    this.$critical    = [0,0,0,0];    // 暴击几率    90 => 暴击率90%
+    this.$dodge       = [0,0,0,0];    // 闪避几率    90 => 闪避率90%
+    this.$coolTimePer = [0,0,0,0];    // 冷却时间减少 10 => 冷却时间减少10%
+    this.$critiDmg    = [0,0,0,0];    // 暴击伤害倍数 1.5 => 1.5倍
+    this.$dmgDown     = [0,0,0,0];    // 伤害减少 [5,10], 免伤 5 + 10%
   }
 
-  action() {
+  powerUp(key, value){
+    for(let i = 0; i< this[key].length; i++){
+      this[key][i] = this[key][i] + value[i];
+    }
+  }
 
+  get maxHp(){
+    const [base, per, _base, _Per] = this.$maxHp;
+    return ((base * (1 + per)) + _base) * (1 + _Per);
+  }
+
+  get maxMp(){
+    const [base, per, _base, _Per] = this.$maxMp;
+    return ((base * (1 + per)) + _base) * (1 + _Per);
+  }
+
+  get str(){
+    const [base, per, _base, _Per] = this.$str;
+    return ((base * (1 + per)) + _base) * (1 + _Per);
+  }
+
+  get dex(){
+    const [base, per, _base, _Per] = this.$dex;
+    return ((base * (1 + per)) + _base) * (1 + _Per);
+  }
+
+  get int(){
+    const [base, per, _base, _Per] = this.$int;
+    return ((base * (1 + per)) + _base) * (1 + _Per);
+  }
+
+  get atk(){
+    const [base, per, _base, _Per] = this.$atk;
+    return (((base + this[this.$attrType]) * (1 + per)) + _base) * (1 + _Per);
   }
 
 }
--- index.js
+++ index.js
@@ -6,11 +6,283 @@ let monster = new Unit({
   tags: [ new Tag('monster'), ]
 })
 
-console.log(hero, monster)
+const SkillList = [
+  {
+    name: '普通攻击',
+    dsc: `对敌人造成100%的伤害`,
+    events: ['1'],
+  },
+  {
+    name: '狂热',
+    dsc: `狂怒下,造成伤害增加30%`,
+    events: ['2'],
+  },
+  {
+    name: '圣光术',
+    dsc: `恢复10%损失的生命值`,
+    events: ['3'],
+  },
+  {
+    id: '100000',
+    name: '剧毒喷雾',
+    dsc: `每秒对敌人造成15%毒属性的伤害，如果对方处于狂热状态, 造成的伤害翻倍`,
+    events: ['4'],
+  },
+  {
+    id: '100000_1',
+    events: ['4_1'],
+  },
+  {
+    id: '100000_2',
+    events: ['4_2'],
+  },
+]
 
-function action(origin, target){
+const Event = [
+  {
+    id: '2',
+    weight: 1,
+    event: function(origin, target){
+      this.origin.powerUp('atk', [0, 0.3, 0, 0]);
+    }
+  },
 
-}
+  {
+    id: '1',
+    weight: 100,
+    event: function(origin, target){
+      return {
+        origin: {},
+        target: {
+          hp: -origin.atk,
+        }
+      }
+    }
+  },
+
+  {
+    id: '3',
+    weight: 100,
+    event: function(origin, target){
+      return {
+        origin: {
+          hp: parseInt((origin.maxHp - origin.hp) * 0.1),
+        },
+        target: {}
+      }
+    }
+  },
+
+  {
+    id: '4',
+    weight: 100,
+    event: function(origin, target){
+      return {
+        origin: {},
+        target: {
+          buffs: [{
+            type: 'ADD',
+            buff: ['1'],
+          }]
+        }
+      }
+    }
+  },
+
+  {
+    id: '4_1',
+    weight: 100,
+    event: function(origin, target){
+      return {
+        origin: {},
+        target: {
+          hp: -parseInt(origin.atk * 0.15),
+        }
+      }
+    }
+  },
+
+  {
+    id: '4_2',
+    weight: 100,
+    event: function(origin, target){
+      return {
+        origin: {},
+        target: {
+          hp: -parseInt(origin.atk * 0.30),
+        }
+      }
+    }
+  },
+
+]
+
+const Buff = [
+  {
+    id: '1',
+    name: '剧毒',
+    status: {},
+    event: function(origin, target){
+      setInterval(() => {
+        if(target.buff.indexOf('100001')){
+          action(origin, target, '100003')
+        }else{
+          action(origin, target, '100004')
+        }
+      })
+    }
+  }
+]
 
 
-action(hero, monster)
+// console.log(hero, monster)
+
+// class Action{
+//   constructor(props){
+//     this.origin = props.origin;
+//     this.target = props.target;
+//     this.skill = props.skill;
+//   }
+
+//   /** Action */
+//   action(){
+//     let skill = {
+//       status: {},
+//       origin_status: {},
+//       initStatus: function(){},
+//       init: function(origin, target){
+//         return {
+//           origin: {},
+//           target: {
+//             attack: origin.$attack
+//           },
+//         }
+//       }
+//     }
+
+//     let EventResult = skill.init(this.origin, this.target);
+
+//     [
+//       this.origin.buffs.filter((i) => i.isInitiative),
+//       this.target.buffs.filter((i) => !i.isInitiative),
+//     ].forEach((buff) => {
+//       buff.event(EventResult, this, buff);
+//     })
+
+//     this.origin.actionUpdate(this.origin);
+//     this.target.actionUpdate(this.target);
+
+//   } /** Action */
+
+
+// }
+
+// new Action(origin, target, '10000').action();
+
+
+// function action(origin, target, skill){
+
+//   let skill = {
+//     status: {},
+//     origin_status: {},
+//     initStatus: function(){},
+//     init: function(origin, target){
+//       return {
+//         origin: Event,
+//         target: Event,
+//       }
+//     }
+//   }
+
+//   let EventResult = skill.init.call(this)
+
+
+// }
+
+// action(origin, target, '10000')
+
+
+
+
+
+
+
+// // 
+//   '10000' InitEvent(origin, target) => {
+//     attack: 100;
+//   } as event
+
+// // Buff
+//   Filter origin => Tag => tags(event, origin, target) => ({
+//     origin : {
+//       attack: 120,
+//     },
+//     target : {
+//       attack: 120,
+//     }
+//   })
+
+//   Filter target => Tag => tags(event, origin, target) => ({
+//     origin : {
+//       attack: 111,
+//     },
+//     target : {
+//       attack: 111,
+//     }
+//   })
+
+//   // Tags;
+//   [
+//     {
+//       $type: 'origin' | 'target',
+//       icon: '毒',
+//       description: '中毒了',
+//       //////////////////////////////////
+//       weight: 0,
+//       status: {},
+//       origin: function(event, origin, target){},
+//       target: function(event, origin, target){},
+//       remove: function(event, origin, target){},
+//       add: function(event, origin, target){},
+//       change: function(){},
+//     }
+//   ]
+
+//   // Skill:
+//   ({
+//     status: {},
+//     origin_status: {},
+//     initStatus: function(){},
+//     init: function(origin, target){
+//       return {
+//         origin: Event,
+//         target: Event,
+//       }
+//     }
+//   })
+
+//   //Event
+//   ({
+//     attacks: [
+//       {attack: 100, attack_type: 'physics'},
+//     ],
+//     tags: [
+//       {
+//         type: 'ADD',
+//         id: '1000001',
+//         status: {level: 3},
+//       },
+//       {
+//         type: 'REMOVE',
+//         id: '1000001',
+//       },
+//       {
+//         type: 'CHANGE',
+//         id: '1000001',
+//         change: function(tag){ return tag},
+//       }
+//     ],
+//     consume: [function(origin, target){
+//       origin.gold -= 100;
+//     }],
+//   })
